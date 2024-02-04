3c58357bd74f7d749bdd290b12077f216283e554
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-04 09:21:09 -0300

    feat: game repository

--- src/main/java/com/boardcamp/api/models/GameModel.java
+++ src/main/java/com/boardcamp/api/models/GameModel.java
@@ -30,7 +30,7 @@ public class GameModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
---
+++ src/main/java/com/boardcamp/api/services/GameService.java
@@ -0,0 +1,23 @@
+package com.boardcamp.api.services;
+
+import org.springframework.stereotype.Service;
+
+import com.boardcamp.api.DTOs.GameDTO;
+import com.boardcamp.api.models.GameModel;
+import com.boardcamp.api.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    private GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public GameModel createGame(GameDTO gameDTO){
+        boolean gameExistByName= gameRepository.existsByName(gameDTO.getName());
+
+
+    }
+}
