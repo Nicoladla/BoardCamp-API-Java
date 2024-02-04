9130652f0103ebe2c3ccaf384a734be28c59d994
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-04 19:51:00 -0300

    feat: DTO for validating customer data

--- src/main/java/com/boardcamp/api/DTOs/CustomerDTO.java
+++ src/main/java/com/boardcamp/api/DTOs/CustomerDTO.java
@@ -1,5 +1,17 @@
 package com.boardcamp.api.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class CustomerDTO {
-    
+
+    @NotBlank(message = "Name cannot be null or empty.")
+    @Size(min = 3,  message = "Name must have more than two characters")
+    private String name;
+
+    @NotBlank(message = "CPF cannot be null or empty.")
+    @Size(min = 1, max = 11,  message = "CPF must have 11 characters")
+    private String cpf;
 }
