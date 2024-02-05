b82a292f13491371f73a45fb3c35ba7122f0506c
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-05 12:44:27 -0300

    feat: validation of the customer's CPF format

--- src/main/java/com/boardcamp/api/DTOs/CustomerDTO.java
+++ src/main/java/com/boardcamp/api/DTOs/CustomerDTO.java
@@ -1,6 +1,7 @@
 package com.boardcamp.api.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -8,10 +9,10 @@ import lombok.Data;
 public class CustomerDTO {
 
     @NotBlank(message = "Name cannot be null or empty.")
-    @Size(min = 3,  message = "Name must have more than two characters")
+    @Size(min = 3, message = "Name must have more than two characters")
     private String name;
 
     @NotBlank(message = "CPF cannot be null or empty.")
-    @Size(min = 1, max = 11,  message = "CPF must have 11 characters")
+    @Pattern(regexp = "^[0-9]{11}$", message = "Invalid CPF format. It must have 11 numeric digits.")
     private String cpf;
 }
