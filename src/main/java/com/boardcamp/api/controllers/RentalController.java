8ed89ee0af6df54b087a198c9ae0aa9776e012a1
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-06 13:15:50 -0300

    feat: rental controller and service

--- src/main/java/com/boardcamp/api/controllers/RentalController.java
+++ src/main/java/com/boardcamp/api/controllers/RentalController.java
@@ -1,5 +1,16 @@
 package com.boardcamp.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boardcamp.api.services.RentalService;
+
+@RestController
+@RequestMapping("/rentals")
 public class RentalController {
-    
+    private RentalService rentalService;
+
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
 }
--- src/main/java/com/boardcamp/api/services/RentalService.java
+++ src/main/java/com/boardcamp/api/services/RentalService.java
@@ -1,5 +1,14 @@
 package com.boardcamp.api.services;
 
+import org.springframework.stereotype.Service;
+
+import com.boardcamp.api.repositories.RentalRepository;
+
+@Service
 public class RentalService {
-    
+    private RentalRepository rentalRepository;
+
+    public RentalService(RentalRepository rentalRepository) {
+        this.rentalRepository = rentalRepository;
+    }
 }
