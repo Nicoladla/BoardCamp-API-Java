d6301565df74628c1fa88453f285348d68c18078
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-08 20:14:40 -0300

    feat: query repository rentals

--- src/main/java/com/boardcamp/api/repositories/RentalRepository.java
+++ src/main/java/com/boardcamp/api/repositories/RentalRepository.java
@@ -1,11 +1,14 @@
 package com.boardcamp.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.boardcamp.api.models.RentalModel;
 
 @Repository
-public interface RentalRepository extends JpaRepository<RentalModel, Long>{
-    
+public interface RentalRepository extends JpaRepository<RentalModel, Long> {
+    @Query(value = "SELECT COUNT(*) FROM rentals WHERE game_id= :id", nativeQuery = true)
+    Long countRentalsPerGame(@Param("id") Long id);
 }
