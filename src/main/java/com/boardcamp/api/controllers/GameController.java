fbddf03ae9115a6f0623c78fc96e86cc94b763cb
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-09 23:08:39 -0300

    feat: cors

--- src/main/java/com/boardcamp/api/controllers/GameController.java
+++ src/main/java/com/boardcamp/api/controllers/GameController.java
@@ -15,9 +15,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("games")
 public class GameController {
@@ -30,11 +31,10 @@ public class GameController {
 
     @GetMapping()
     public ResponseEntity<List<GameModel>> getGames() {
-        List<GameModel> games= gameService.findGames();
-        
+        List<GameModel> games = gameService.findGames();
+
         return ResponseEntity.status(HttpStatus.OK).body(games);
     }
-    
 
     @PostMapping
     public ResponseEntity<GameModel> postGame(@RequestBody @Valid GameDTO gameDTO) {
