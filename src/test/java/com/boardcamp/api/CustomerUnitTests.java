4ffca2b2ba34618ef51a77c602a14abd1d208689
Nicolas Araújo <nicolasaraujo04@gmail.com>
2024-02-09 22:59:12 -0300

    test: unit test for a service where the user can get a specific client

---
+++ src/test/java/com/boardcamp/api/CustomerUnitTests.java
@@ -0,0 +1,40 @@
+package com.boardcamp.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.boardcamp.api.models.CustomerModel;
+import com.boardcamp.api.repositories.CustomerRepository;
+import com.boardcamp.api.services.CustomerService;
+
+@SpringBootTest
+public class CustomerUnitTests {
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Test
+    void givenAValidCustomerId_whenWantFindCustomer_thenReturnCustomer() {
+        Long id = 1L;
+        CustomerModel customer = new CustomerModel(id, "name", "12345678900");
+
+        doReturn(Optional.of(customer)).when(customerRepository).findById(any());
+
+        CustomerModel result = customerService.findCustomerById(id);
+
+        verify(customerRepository, times(1)).findById(any());
+        assertEquals(customer, result);
+    }
+}
